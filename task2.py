3f1f1e1d996951ed59da38124ac5b472d6cae1d5
GANGSTERS7590 <67689741+GANGSTERS7590@users.noreply.github.com>
2025-08-04 12:57:09 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,6 @@
+# Task 2: Personalized Greeting
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print(f"\nHello, {first_name} {last_name}! Welcome to the Python program.")
